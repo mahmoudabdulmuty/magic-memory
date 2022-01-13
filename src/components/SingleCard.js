add514fe807aca4877ac7f2f1c1e226389482db8
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-14 00:36:17 +0200

    update classNames in card component

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -5,7 +5,12 @@ export default function SingleCard({ src, handleClick, flipped }) {
 		<div className="card">
 			<div className={flipped ? 'flipped' : ''}>
 				<img className="front" src={src} alt="front card" />
-				<img src="./img/cover.png" onClick={handleClick} alt="back card" />
+				<img
+					className="back"
+					src="./img/cover.png"
+					onClick={handleClick}
+					alt="back card"
+				/>
 			</div>
 		</div>
 	);
